ba8e3f1c7950b7c2142248cc74304f18a195add5
“gihzq <614977099@qq.com>
2020-07-17 16:06:57 +0800

    eslint

--- src/js/app.js
+++ src/js/app.js
@@ -15,3 +15,18 @@ console.log(test)
 // 引入less
 import "../less/test1.less"
 import "../less/test2.less"
+
+
+
+window.love = "love"
+global.love = "love"
+
+// $ = 100
+$("body").css("background","red")
+
+let a = 100
+if(a == 100){
+  console.log("正确的")
+}
+
+alert("我就alert")
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   // 配置规则
   module: {
     rules: [
+      // less文件编译
       {
         test: /\.less$/,  		// 检查文件是否以.less结尾（检查是否是less文件）
         use: [					// 数组中loader执行是从下到上，从右到左顺序执行
@@ -18,6 +19,15 @@ module.exports = {
           'css-loader', 		// 将css以commonjs方式整合到js文件中
           'less-loader' 		// 将less文件解析成css文件
         ]
+      },
+      // eslint 语法检查
+      {
+        test: /\.js$/,                  //只检测js文件
+        exclude: /node_modules/,        //排除node_modules文件夹
+        enforce: "pre",                 //提前加载使用
+        use: {
+          loader: "eslint-loader"		//使用eslint-loader解析
+        }
       }
     ]
   },
